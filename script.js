c4ea1d8009c37ed056de1258207ac58b0de51636
GabyLinares <gabilina20@gmail.com>
2023-10-24 14:58:50 -0500

    Finished

---
+++ script.js
@@ -0,0 +1,34 @@
+const diasQ = document.getElementById('dias')
+const horasQ = document.getElementById('horas')
+const minsQ = document.getElementById('minutos')
+const secsQ = document.getElementById('segundos')
+
+const fechaQuery = "3 Dec 2023";
+
+formatoCero = (elemento) => {
+
+    return elemento < 10 ? `0${elemento}` : elemento;
+
+}
+
+countdown = () => {
+    const fechaBusqueda = new Date(fechaQuery);
+    const fechaHoy = new Date();
+
+    const secsFaltantes = (fechaBusqueda - fechaHoy) / 1000;
+
+    // console.log(secsFaltantes);
+
+    const dias = Math.floor(secsFaltantes / 3600 / 24);
+    const horas = Math.floor(secsFaltantes / 3600) % 24;
+    const mins = Math.floor(secsFaltantes / 60) % 60;
+    const secs = Math.floor(secsFaltantes) % 60;
+
+    diasQ.innerHTML = dias;
+    horasQ.innerHTML = formatoCero(horas);
+    minsQ.innerHTML = formatoCero(mins);
+    secsQ.innerHTML = formatoCero(secs);
+}
+
+countdown();
+setInterval(countdown, 1000);
